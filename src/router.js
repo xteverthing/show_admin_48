e12fbdc8f2a4cdad30df7ba539b0035cafdbb927
xteverthing <2275280938@qq.com>
2019-09-28 20:51:46 +0800

    划分项目目录完成

--- src/router.js
+++ src/router.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login.vue'
 import Index from './components/Index.vue'
-import Users from './components/Users.vue'
-import Roles from './components/Roles.vue'
-import Rights from './components/Rights.vue'
+import Users from './components/users/Users.vue'
+import Roles from './components/rights/Roles.vue'
+import Rights from './components/rights/Rights.vue'
+import Categories from './components/products/Categories.vue'
 Vue.use(Router)
 // 解决 vue-router 版本更新小bug(不用记)
 const originalPush = Router.prototype.push
@@ -24,7 +25,9 @@ const router = new Router({
       children: [
         { path: '/users', component: Users },
         { path: '/roles', component: Roles },
-        { path: '/rights', component: Rights }
+        { path: '/rights', component: Rights },
+        { path: '/categories', component: Categories }
+
       ]
 
     },
